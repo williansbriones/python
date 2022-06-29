9445199aa114a2dc85379d4ca3b01d957f706c2a
williansbriones <wbriones2013@gmail.com>
2022-06-29 06:48:03 +0000

    ayer

--- eva4.2..py
+++ eva4.2..py
@@ -1,5 +1,7 @@
 import numpy as np
+tabla=""
 acum=0
+acum_c=0
 asientos_normal=np.ones([5,6],dtype="int32")
 for i in range(5):
     for e in range(6):
@@ -10,16 +12,23 @@ for i in range(2):
     for e in range(6):
         asientos_vip[i][e]=asientos_vip[i][e]+acum
         acum=acum+1
+total_asientos_n=np.concatenate((asientos_normal,asientos_vip),axis=0,dtype="int32")
 total_asientos=np.concatenate((asientos_normal,asientos_vip),axis=0,dtype="str")
 print(total_asientos)
+total_asientos[1][1]="x"
 for i in range(7):
             for e in range(6):
+                acum_c=acum_c+1
                 if total_asientos[i][e].isnumeric():
-                    numero=total_asientos[i][e]
-                    int=numero
+                    numero=total_asientos_n[i][e]
+                    
                     if numero < 10:
                         tabla=tabla + "| "+ total_asientos[i][e]
                     if numero > 9:
-                        tabla=tabla + "| "+ total_asientos[i][e]
+                        tabla=tabla + "|"+ total_asientos[i][e]
                 else:
                     tabla=tabla + "| "+ total_asientos[i][e]
+                if acum_c==6:
+                    acum_c=0
+                    tabla=tabla + "\n"
+print(tabla)
